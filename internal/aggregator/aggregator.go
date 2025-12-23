c8b65671c1211fbc0ffb53163a794a756a8b6a36
Narvdeshwar <narvdeshwar@xenelsoft.co.in>
2025-12-23 09:47:49 +0530

    feat: implement Run method for Aggregator to process log streams

--- internal/aggregator/aggregator.go
+++ internal/aggregator/aggregator.go
@@ -1,5 +1,11 @@
 package aggregator
 
+import (
+	"context"
+
+	"github.com/Narvdeshwar/go-logstreamer/pkg/model"
+)
+
 type Aggregator struct {
 	totalLines   int
 	levelCount   map[string]int
@@ -10,5 +16,21 @@ func NewAggregator() *Aggregator {
 	return &Aggregator{
 		levelCount:   make(map[string]int),
 		serviceCount: make(map[string]int),
+	}
+}
+
+func (a *Aggregator) Run(ctx context.Context, in <-chan model.LogEntry) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case entry, ok := <-in:
+			if !ok {
+				return
+			}
+			a.totalLines++
+			a.levelCount[entry.Level]++
+			a.serviceCount[entry.Service]++
+		}
 	}
 }
