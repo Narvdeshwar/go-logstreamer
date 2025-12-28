470c3642ddc0225151a692e7233cbdd6bbde65fd
narvdeshwar <sam.07soni@gmail.com>
2025-12-28 12:19:58 +0530

    feat: Add graceful shutdown with signal logging

--- cmd/logstreamer/main.go
+++ cmd/logstreamer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,13 +12,14 @@ import (
 )
 
 func main() {
-	cfg := config.Load() // loading the file with [file_address,workers,buffer
+	cfg := config.Load() // loading the file with [file_address,workers,buffer]
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		<-sigCh
+		sig := <-sigCh
+		log.Println("Received Signal: ", sig)
 		cancel()
 	}()
 	p := pipeline.New(cfg) // this will return the cfg file address so that single source of data can be maintain
